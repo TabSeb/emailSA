3e9634a196fa29e9c2d8593bff29dafa31f3fa1b
TabSeb <sebtaborda08@gmail.com>
2022-06-15 11:58:49 -0300

    arreglar bug: esconder el mensaje de error al enviar el email

--- emailSA/Form1.cs
+++ emailSA/Form1.cs
@@ -54,6 +54,7 @@ namespace emailSA
                 usuario.cuerpo = ingresarCuerpoText.Text;
                 usuario.enviar();
                 exitoMensaje.Show();
+                errorCorreo.Hide();
 
             }
             catch (Exception)
